86a5b552e5724a442587d8f6fbbe12cdbb040c13
Vladimir <70746959+VladimirKrivov@users.noreply.github.com>
2022-10-12 13:03:27 +0300

    Create project

---
+++ src/main/java/com/example/dao/spring/SpringDaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dao.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDaoApplication.class, args);
+	}
+
+}
